b34ef39428fc3a73dd98592df4850147b79820f1
AHMahi <abdulhamidmahi2000@gmail.com>
2023-06-17 21:44:22 +1000

    3rd change

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,7 @@ function Home() {
     <div className='home-container'>
       <h1>FRESH GROCERIES AWAIT</h1>
 <h2>hey therer</h2>
+<h3>gsdg</h3>
       <div className='home-btns'>
         <button className="nav-items-list">
           <Link to="/delivery">GET STARTED</Link>
